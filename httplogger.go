7e5a09b2e1d9f0a4ac08d5fa7f4f0f0fc14a0654
Ernesto Jiménez <me@ernesto-jimenez.com>
2014-09-24 23:24:23 +0100

    Add DefaultLogger and DefaultLoggedTransport

--- httplogger.go
+++ httplogger.go
@@ -1,6 +1,7 @@
 package httplogger
 
 import (
+	"log"
 	"net/http"
 	"time"
 )
@@ -29,3 +30,20 @@ type HTTPLogger interface {
 	LogRequest(*http.Request)
 	LogResponse(response *http.Response, err error, duration time.Duration)
 }
+
+// DefaultLogger is an http logger that will use the standard logger in the log package to provide basic information about http responses
+type DefaultLogger struct {
+}
+
+// LogRequest doens't do anything since we'll be logging replies only
+func (dl DefaultLogger) LogRequest(*http.Request) {
+}
+
+// LogResponse logs path, host, status code and duration in milliseconds
+func (dl DefaultLogger) LogResponse(res *http.Response, err error, duration time.Duration) {
+	duration /= time.Millisecond
+	log.Printf("HTTP Request host=%s method=%s status=%d durationMs=%d", res.Request.Host, res.Request.Method, res.StatusCode, duration)
+}
+
+// DefaultLoggedTransport wraps http.DefaultTransport to log using DefaultLogger
+var DefaultLoggedTransport = NewLoggedTransport(http.DefaultTransport, DefaultLogger{})
